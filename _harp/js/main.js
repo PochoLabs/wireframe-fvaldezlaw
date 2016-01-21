7712f03cfc3cd59a4788df917c3c0bca4e28ae36
Celso Mireles <celso@digitalstrategy.tips>
2016-01-20 20:55:46 -0700

    Fix nav links

--- _harp/js/main.js
+++ _harp/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	var domain = $(location).attr('hostname');
-	
+
 	if(domain === 'localhost'){
 		$('.home-nav > li > a').each(function(){
-			var newLink = $(this).attr('href').slice(8);
+			var newLink = $(this).attr('href').slice(22);
 			$(this).attr('href', newLink);
 		});
 	}
