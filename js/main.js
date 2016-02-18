e63c50c171da27413890ca96d1e34b4be6dc3f8f
Celso Mireles <celso@digitalstrategy.tips>
2016-02-18 13:55:38 -0700

    Make .feature-gradient full height on home page.

--- js/main.js
+++ js/main.js
@@ -13,4 +13,12 @@ $(document).ready(function(){
 		});
 	}
 
+	// Make .feature-bg be full height
+
+	var wHeight = $(window).height();
+
+	console.log("Window Height is: " + wHeight);
+
+	$('.feature-gradient').css('min-height', wHeight + 'px');
+
 });
