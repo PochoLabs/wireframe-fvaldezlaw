a9dd678b1228d2c7e8bf998c996da4d58bd90689
Celso Mireles <celso@digitalstrategy.tips>
2016-02-18 14:38:15 -0700

    Center text appears correctly in xs view

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+	new WOW().init();
+
 	var domain = $(location).attr('hostname');
 
 	if(domain === 'localhost'){
@@ -13,12 +16,25 @@ $(document).ready(function(){
 		});
 	}
 
-	// Make .feature-bg be full height
+	// Make .feature-gradient be full height
 
 	var wHeight = $(window).height();
 
-	console.log("Window Height is: " + wHeight);
-
 	$('.feature-gradient').css('min-height', wHeight + 'px');
+
+	// Animation for animated links
+
+	$('.scroll-link').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 
 });
